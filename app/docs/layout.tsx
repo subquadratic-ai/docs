2296db95fc662c98f8fe16b72fc47f847798c6cf
Dickson Mwendia <64727760+Dickson-Mwendia@users.noreply.github.com>
2026-03-02 17:54:00 +0300

    doc updates

--- app/docs/layout.tsx
+++ app/docs/layout.tsx
@@ -7,7 +7,6 @@ export default function Layout({ children }: LayoutProps<"/docs">) {
     <DocsLayout
       tree={source.pageTree}
       {...baseOptions()}
-      tabMode="top"
       sidebar={{
         defaultOpenLevel: 2,
         collapsible: true,
